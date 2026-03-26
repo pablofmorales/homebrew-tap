18e52251edad8fd55691959d6027816d930e2ef1
Pablo Morales <pablofmorales@gmail.com>
2026-03-26 15:35:32 -0300

    feat: add mac-cleaner formula v1.5.10

---
+++ Formula/mac-cleaner.rb
@@ -0,0 +1,19 @@
+class MacCleaner < Formula
+  desc "Fast, safe CLI for cleaning macOS development caches"
+  homepage "https://github.com/BlackAsteroid/mac-cleaner-cli"
+  url "https://registry.npmjs.org/@blackasteroid/mac-cleaner-cli/-/mac-cleaner-cli-1.5.10.tgz"
+  sha256 "944c91f628e4fc93b0056e59d79c243ecd51b45ebf97164f878609070459764f"
+  license "MIT"
+  version "1.5.10"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mac-cleaner --version")
+  end
+end
